e4884225ed50854562432fc37180e278a0479ea6
Dmitriy Muzyka <d.muzyka@propellerads.net>
2017-07-31 15:22:08 +0300

    pg log parser

---
+++ pg_log_parser.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import sys
+import os
+import re
+
+in_file = sys.argv[1]
+
+#2017-07-27 00:00:05.469 EST [83200] web@192.168.21.53 postgres
+# '%m [%p] %q%u@%h %d '
+
+llp = '%m [%p] %q%u@%h %d '
+
+m_re = '(?P<date>\d{4}(\-\d{2}){2}) (?P<time>(\d{2}:?){3}\.\d+) (?P<timezone>\w+)'
+p_re = '(?P<pid>\d+)'
+q_re = ''
+u_re = '(?P<user>\w+)'
+h_re = '(?P<ip>\S+)'
+d_re = '(?P<database>(\w+|\[unknown\]))'
+
+log_type_re = '(?P<log_type>\w+): '
+duration_re = ' duration: (?P<query_time>\d+\.\d+) ms'
+
+new_records = ['LOG', 'ERROR', 'FATAL']
+
+def escape_re(string):
+    for i in ['\\', '[', ']', '+']:
+        string = string.replace(i, "\\{}".format(i))
+    return string
+
+def convert_to_re(in_llp):
+    in_llp = escape_re(in_llp)
+    re = in_llp.replace('%m', m_re).replace('%p', p_re).replace('%q', q_re).replace('%u', u_re).replace('%h', h_re).replace('%d', d_re)
+    re += log_type_re
+    return '^{}'.format(re)
+
+def process_query(query):
+    print query
+    print '------------'
+
+def parse_log(file):
+    query = ''
+    slow = False
+    current_log_type = 'LOG'
+
+    for line in file.readlines():
+        parsed = new_duration_log_line_pattern.search(line) or new_log_line_pattern.search(line) or new_non_session_log_line_pattern.search(line)
+        if  parsed:
+            log_type =  parsed.group('log_type')
+            if log_type in new_records:
+                if slow:
+                    #process_query(query)
+                    slow = False
+                if current_log_type == 'ERROR':
+                    process_query(query)
+                    pass
+                new_block = True
+                query = line
+                current_log_type = log_type
+                if 'query_time' in parsed.groupdict():
+                    query_time = float(parsed.group('query_time'))
+                    if query_time > 100000:
+                        slow = True
+            else:
+                query += line
+        else:
+            query += line
+
+
+
+
+
+non_session_llp = llp.split('%q')[0]
+duration_llp = llp + duration_re
+
+non_session_llp_re = convert_to_re(non_session_llp)
+llp_re = convert_to_re(llp)
+duration_llp_re = llp_re + duration_re
+
+new_non_session_log_line_pattern  = re.compile(non_session_llp_re)
+new_log_line_pattern = re.compile(llp_re)
+new_duration_log_line_pattern = re.compile(duration_llp_re)
+
+last_position = 1
+with open(in_file) as f:
+    f.seek(last_position)
+    parse_log(f)
+
+
