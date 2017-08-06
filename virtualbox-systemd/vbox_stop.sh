4fa39f35b7b8cbf3d87bdd5ce18e084a992f1d97
Dmitriy Muzyka <a5b1ca@gmail.com>
2017-08-06 16:04:42 +0300

    vbox unit file

---
+++ virtualbox-systemd/vbox_stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+UUID=$1
+
+
+nVM=$(/usr/bin/vboxmanage list runningvms | grep -c ${UUID})
+
+while [ ${nVM} -ne 0 ]; do
+	/usr/bin/vboxmanage controlvm ${UUID} acpipowerbutton
+    nVM=$(/usr/bin/vboxmanage list runningvms | grep -c ${UUID})
+    sleep 1;
+done
+exit 0
+
