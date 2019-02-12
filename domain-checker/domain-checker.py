83962fd3b08df9325c37c6952f7342727573503c
Dmitriy Muzyka <a5b1ca@gmail.com>
2019-02-13 02:22:48 +0300

    docmain checker

--- domain-checker/domain-checker.py
+++ domain-checker/domain-checker.py
@@ -8,6 +8,7 @@ import os
 
 import socket
 import ssl
+import argparse
 
 import send_mail as notify
 
@@ -33,22 +34,33 @@ def domain_expiry_days(domain):
     expiration_date = parsed['expiration_date'][0]
     return (expiration_date - datetime.datetime.now()).days
 
+
+def parse_arg():
+    parser = argparse.ArgumentParser(description='domain and ssl exipy checker.')
+    parser.add_argument('domains', type=str, help='domains to process', nargs='+')
+    parser.add_argument('--check', '-c', type=str, help='check ssl or domain')
+    parser.add_argument('--recipient', '-r', type=str, help='recipient', nargs='+')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("Usage: {} <discovery|status|nodeStatus> [serviceID]".format(sys.argv[0]))
-        sys.exit(1)
-    action = sys.argv[1].lower()
-    domain = sys.argv[2]
-    if action == 'domain':
-        remaining_days = domain_expiry_days(domain)
-        msg = "domain {} expired in {} days".format(domain, remaining_days)
-    elif action == 'ssl':
-        remaining_days = ssl_expiry_days(domain)
-        msg = "ssl on {} expired in {} days".format(domain, remaining_days)
-    if remaining_days < 14 or True:
-        print(msg)
-        recipient = 'a5b1ca@gmail.com'
-        notify.send_mail(recipient, 'domain checkert alert', msg)
+    args = parse_arg()
+    check = args.check
+    domains = args.domains
+    recipients = args.recipient
+    for domain in domains:
+        if check == 'domain':
+            remaining_days = domain_expiry_days(domain)
+            msg = "domain {} expired in {} days".format(domain, remaining_days)
+        elif check == 'ssl':
+            remaining_days = ssl_expiry_days(domain)
+            msg = "ssl on {} expired in {} days".format(domain, remaining_days)
+        if remaining_days < 14:
+            print(msg)
+            for recipient in recipients:
+                print(recipient)
+                notify.send_mail(recipient, 'domain checkert alert', msg)
 
 
 
