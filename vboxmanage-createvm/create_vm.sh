228018e090098a8e7192af0befef4119e8f0a038
Dmitry Muzyka <a5b1ca@gmail.com>
2019-11-17 00:24:03 +0300

    create vm via virtualboxmanage from console template

---
+++ vboxmanage-createvm/create_vm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+vmname=$1
+# vboxmanage list ostypes
+ostype=Windows2016_64
+
+vboxmanage createvm --name "${vmname}" --ostype "$ostype" --register
+vboxmanage modifyvm "${vmname}" --cpus 6 --memory 16000
+vboxmanage modifyvm "${vmname}" --accelerate2dvideo on
+vboxmanage modifyvm "${vmname}" --vrde on --vrdeauthtype=null --vrdeaddress=10.40.0.1 --vrdeport=3391
+vboxmanage storagectl "${vmname}" --name 'SATA Controller' --add sata
+vboxmanage modifyvm "${vmname}" --nic1 hostonly
+vboxmanage modifyvm "${vmname}" --audio none
+
+vboxmanage createmedium disk --filename /root/VirtualBox\ VMs/"${vmname}"/"${vmname}".vdi --format vdi  --size $((1024*50))
+vboxmanage list hdds
+vboxmanage storageattach  "${vmname}" --storagectl 'SATA Controller' --port 0 --medium a7ea3581-bfc3-4a98-9e49-63db3cf1ccf4  --type hdd
