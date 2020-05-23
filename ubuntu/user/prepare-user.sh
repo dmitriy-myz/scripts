1b22fbd6047f2da0ed5aa0a813e4547c059a4f14
Dmitry Muzyka <a5b1ca@gmail.com>
2020-05-23 16:52:38 +0300

    prepare user

--- ubuntu/user/prepare-user.sh
+++ ubuntu/user/prepare-user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # restore dconf settings like hot cornets; week days, etc
-cat "$(dirname $0)/user/dconf | dconf load /
+cat "$(dirname $0)/user/dconf" | dconf load /
 
 mv "~/.bashrc" "~/.bashrc-$(date +%s)"
 cp "$(dirname $0)/user/bashrc" "~/.bashrc"
