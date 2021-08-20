d7d9bcc04a44ceaad2674b9f7a8a158301e6653c
Dmitry Muzyka <a5b1ca+git@gmail.com>
2021-08-20 20:40:47 +0300

    nmap project

--- nmap_project/scan_hosts.py
+++ nmap_project/scan_hosts.py
@@ -63,7 +63,7 @@ alert_msg = ''
 for host in nm.all_hosts():
     for proto in nm[host].all_protocols():
         lport = nm[host][proto].keys()
-        lport.sort()
+        lport = sorted(lport)
         disallowed_ports = [item for item in lport if item not in allowed_ports]
         for port in disallowed_ports:
             if nm[host][proto][port]['state'] not in ['closed', 'filtered']:
